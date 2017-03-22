06c7666c4efc793bb08489e29cd1c50db739e53a
dshovchko <d.shovchko@gmail.com>
2017-03-22 16:47:49 +0200

    add the use of the configuration file

--- deny-hosts.pl
+++ deny-hosts.pl
@@ -2,15 +2,20 @@
 
 use Sys::Syslog;
 
-$from = "admin\@myserver.com";
-$to = "admin\@myserver.com";
 %ban;
 %banned;
 
+# Get our configuration information
+if (my $err = ReadCfg('deny-hosts.cfg')) {
+    print(STDERR $err, "\n");
+    exit(1);
+}
+
 openlog("deny-hosts", "ndelay,pid", "local0");
-open (FIFO, "/root/bin/deny-hosts/auth.info");
+open (FIFO, $CFG::CFG{'channel'});
 
 syslog(LOG_INFO, "started");
+smtp_send("started");
 
 while (1) {
     while (<FIFO>) {
@@ -45,16 +50,45 @@ syslog(LOG_INFO, "exited");
 close (FIFO);
 closelog();
 
+#   Read a configuration file
+#   The arg can be a relative or full path, or
+#   it can be a file located somewhere in @INC.
+sub ReadCfg
+{
+    my $file = $_[0];
+
+    our $err;
+
+    {   # Put config data into a separate namespace
+        package CFG;
+
+        # Process the contents of the config file
+        my $rc = do($file);
+
+        # Check for errors
+        if ($@) {
+            $::err = "ERROR: Failure compiling '$file' - $@";
+        } elsif (! defined($rc)) {
+            $::err = "ERROR: Failure reading '$file' - $!";
+        } elsif (! $rc) {
+            $::err = "ERROR: Failure processing '$file'";
+        }
+    }
+
+    return ($err);
+}
+
+
 sub smtp_send {
     my $body = $_[0];
-
+    
     my $time = time();
     my ($sec,my $min,my $hour,my $mday,my $mon,my $year,my $wday,my $yday,my $isdst) = localtime($time);
     my $now = sprintf("%04d-%02d-%02d %02d:%02d:%02d ", ($year+1900), ($mon+1), $mday, $hour, $min, $sec);
-
-    if (open (SENDMAIL, "|/usr/sbin/sendmail -t")) {
-        print SENDMAIL "From: $from\n";
-        print SENDMAIL "To: $to\n";
+    
+    if (open (SENDMAIL, "|".$CFG::CFG{'mail'}{'mailer'})) {
+        print SENDMAIL "From: ".$CFG::CFG{'mail'}{'from'}."\n";
+        print SENDMAIL "To: ".$CFG::CFG{'mail'}{'to'}."\n";
         print SENDMAIL "Subject: IP banned $now\n\n";
         print SENDMAIL "$body";
         close (SENDMAIL);
